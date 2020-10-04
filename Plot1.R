31fa5ae44b82dec4e2f805055140cea212ca3866
juanpmar <juan.marino@correo.usa.edu.co>
2020-10-03 22:48:57 -0500

    Task Complete All plots
    the data should be downloaded from the link on the Readme Explanation, the figures of the task are in the current folder

---
+++ Plot1.R
@@ -0,0 +1,12 @@
+##Juan Pablo Mariño Ardila
+#03/10/2020
+plot1 <-function(){
+data <- read.table("household_power_consumption.txt",sep = ";",header = TRUE,na.strings = "?")
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+data$Global_ative_power <- as.numeric(data$Global_active_power)
+s <- (data$Date == "2007-02-01" | data$Date == "2007-02-02")
+i_data <- data[s,]
+png("PLOT1.png")
+hist(i_data$Global_active_power,col = "red",xlab = "Global Active Power (kilowatts)",main = "Global Active Power")
+dev.off()
+}
